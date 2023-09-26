82d945aab626d11ccb3dbd48f077a4900d4aae39
eownxn <intezya@gmail.com>
2023-09-26 15:10:32 +0400

    table added

---
+++ chess_table.py
@@ -0,0 +1,13 @@
+table = ([
+    # Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook
+    [3, 5, 7, 9, 11, 7, 5, 3],
+    [1, 1, 1, 1, 1, 1, 1, 1],  # Pawns
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [2, 2, 2, 2, 2, 2, 2, 2],  # Pawns
+    # Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook
+    [4, 6, 8, 10, 12, 8, 6, 4]
+])
